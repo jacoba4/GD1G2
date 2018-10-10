d984b8c4f4766cfadc7904e77050b8598c2757ea
Unknown <jacoba4@rpi.edu>
2018-10-10 16:32:14 -0400

    Game now starts in main menu

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -4,8 +4,13 @@ let mainMenuState = function(){};
 mainMenuState.prototype.create = function () {
   //add menu buttons, animations, and title music
   game.add.sprite(0,0,"title");
+  button = game.add.button(game.world.centerX, game.world.centerY, "playbutton", ButtonClick,this);
 };
 
 mainMenuState.prototype.update = function () {
   //display animations if we want them / check for clicks and stuff
 };
+
+function ButtonClick(){
+	game.state.start("Fight");
+}
