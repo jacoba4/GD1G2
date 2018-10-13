8b45dabb12ec66a696255b076eb07b78b60b1b32
Jessica Lichter <jlichter123@gmail.com>
2018-10-13 19:18:43 -0400

    added title

--- js/preload.js
+++ js/preload.js
@@ -5,6 +5,7 @@ preloadSate.prototype.preload = function () {
   game.load.image("shop","assets/backgrounds/shop_bg.png");
   game.load.image("fight","assets/backgrounds/fight_bg.png");
   game.load.image("victory","assets/backgrounds/victory_bg.png");
+  game.load.bitmapFont('ancientFont', 'assets/fonts/ancientFont.png', 'assets/fonts/ancientFont.fnt');
 };
 
 preloadSate.prototype.create = function () {
