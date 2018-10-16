1de5749947093ee173cc63fe7884bff8b16feb95
Jessica Lichter <jlichter123@gmail.com>
2018-10-16 00:42:06 -0400

    working on title screen

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -4,7 +4,7 @@ let mainMenuState = function(){};
 mainMenuState.prototype.create = function () {
   //add menu buttons, animations, and title music
   game.add.sprite(0,0,"title");
-  button = game.add.button(game.world.centerX, game.world.centerY, "playbutton", ButtonClick,this);
+  //button = game.add.button(game.world.centerX, game.world.centerY, "playbutton", ButtonClick,this);
 };
 
 mainMenuState.prototype.update = function () {
@@ -12,5 +12,5 @@ mainMenuState.prototype.update = function () {
 };
 
 function ButtonClick(){
-	game.state.start("Fight");
+	//game.state.start("Fight");
 }
